0067816f12102d78a89f6f2785554ff3ea88f11a
Mark Nichols <mhn@ksu.edu>
2009-10-04 20:27:31 -0500

    modified notfound.php for a better 404 experience

--- notfound.php
+++ notfound.php
@@ -1,12 +1,22 @@
 <div class="post" id="post-<?php the_ID(); ?>">
 	<div class="posttitle">
-		<h2>Ooops...Where did you get such a link ?</h2>
-		<p class="post-info">Server cannot locate what you are looking for !</p>
+		<h2>404</h2>
+		<p class="post-info">We're sorry. The page you requested isn't available.</p>
 	</div>
 	<div class="entry">
-		<p>The Server tried all of its options before returning this page to you.</p>
-		<p><img src="<?php bloginfo('stylesheet_directory') ;?>/img/404.gif" alt="404" class="left" />You are looking for something that is not here now.<br/>
-			You can always try doing a <strong>search</strong> or browsing through the <strong>Archives</strong>.<br/>Don't loose your hope just yet.<br style="clear:both" /></p>
+		<p>
+			May I suggest you visit the <a href="http://zanshin.net/archives" title="Archives">archives</a>, or 
+			perhaps the <a href="http://zanshin.net" title="Zanshin.net">home page</a>.
+		<p>
+		<p>
+			You could also try doing a search.
+		</p>
+		<p>
+			<form method="get" id="searchform" action="<?php bloginfo('home'); ?>">
+				<input type="text" class="textbox" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="s" />
+				<input type="submit" id="searchsubmit" value="Search" />
+			</form>
+		</p>
 	</div>
 	<p class="postmetadata">Posted as Not Found</p>
 </div>	
